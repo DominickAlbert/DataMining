0254b170f12bc4c005c5432ed019f1001e02149d
Jefry Christiawan <jefrychristiawan2005@gmail.com>
2024-09-21 19:58:22 +0700

    Preparation Data Before Integration

--- DM.R
+++ DM.R
@@ -2,36 +2,26 @@ library(dplyr)
 
 bankingDT_Original <- read.csv("Bangking_Dataset_train.csv")
 marketingDT_Original <- read.csv("Marketing_Target_train.csv", sep = ";")
+bankingDT_Processed <- bankingDT_Original
+marketingDT_Processed <- marketingDT_Original
 
 # rm()
-
-# head(bankingDT_Original)
-# head(marketingDT_Original)
-
-# unique(bankingDT['job'])
-# unique(marketingDT['job'])
 
 # Preparing the same columns that exist on both datasets
 same_columns <- intersect(names(bankingDT_Original), names(marketingDT_Original))
 
 # Present information regarding that dataset
+# head(bankingDT_Original)
+# head(marketingDT_Original)
+
+# unique(bankingDT_Original$job)
+# unique(marketingDT_Original$job)
+
 str(bankingDT_Original[same_columns])
 str(marketingDT_Original[same_columns])
 
-# unique(bankingDT_Original['education'])
-# unique(marketingDT_Original['education'])
-
-# Changing the values on MarketingDT so the education matches the one in the other dataset
-marketing_education_mapped <- marketingDT_Original %>%
-  mutate(education = recode(education,
-                            tertiary = "university.degree",
-                            secondary = "high.school",
-                            primary = "basic.4y",  # Adjust if necessary
-                            unknown = "unknown")
-        )
-
-# str(bankingDT_Original[same_columns])
-# str(marketing_education_mapped[same_columns])
+unique(bankingDT_Original['education'])
+unique(marketingDT_Original['education'])
 
 # unique(bankingDT_Original['marital'])
 # unique(marketingDT_Original['marital'])
@@ -42,25 +32,42 @@ marketing_education_mapped <- marketingDT_Original %>%
 # unique(bankingDT_Original['poutcome'])
 # unique(marketingDT_Original['poutcome'])
 
-# Changing nonexistent into unkown so the data integration can go smooth
-bangkinDT_poutcome_mapped <- bankingDT_Original %>%
-  mutate(poutcome = recode(poutcome,
-                           nonexistent = "unkown"))
-
-# unique(bangkinDT_poutcome_mapped['poutcome'])
-# unique(marketing_education_mapped['poutcome'])
-
-str(bangkinDT_poutcome_mapped[same_columns])
-str(marketing_education_mapped[same_columns])
-
 # unique(bankingDT_Original['pdays'])
 # unique(marketingDT_Original['pdays'])
 
 # unique(bankingDT_Original['previous'])
 # unique(marketingDT_Original['previous'])
 
-# Subsetting the dataset so it only have the same columns
-bangkinDT_transform <- bangkinDT_poutcome_mapped[, same_columns] 
-marketing_transform <- marketing_education_mapped[, same_columns] 
+#--------------- PREPARATION BEFORE INTEGRATION -----------------------
 
-combined_data <- rbind(bangkinDT_transform,marketing_transform)
+# Changing the values on BankingDT so the education matches the one in the other dataset
+
+bankingDT_Processed <- bankingDT_Processed %>%
+  mutate(education = recode(education,
+                            basic.4y = "primary",
+                            basic.6y = "primary",
+                            basic.9y = "secondary",
+                            high.school = "secondary",
+                            professional.course = "tertiary",
+                            university.degree = "tertiary",
+                            illiterate = "unknown",
+                            unknown = "unknown")
+        )
+
+# Changing the value -1 of pdays in marketingDT_Processed 
+# because marketingDT_Processed value -1 means not previously contacted
+# however in bankingDT_Processed previously contacted is assigned as value 999
+marketingDT_Processed <- marketingDT_Processed %>%
+  mutate(pdays = ifelse(pdays == -1, 999, pdays))
+
+# Changing nonexistent into unknown so the data integration can go smooth
+bankingDT_Processed <- bankingDT_Processed %>%
+  mutate(poutcome = recode(poutcome,
+                            nonexistent="unknown")
+  )
+
+# Subsetting the dataset so it only have the same columns
+bankingDT_Processed <- bankingDT_Processed[, same_columns] 
+marketingDT_Processed <- marketingDT_Processed[, same_columns] 
+
+dataset <- rbind(bankingDT_Processed,marketingDT_Processed)
