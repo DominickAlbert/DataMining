548a6caf74ed7d9cec7b592b7f27a926b53c3e96
Jefry Christiawan <jefrychristiawan2005@gmail.com>
2024-11-30 13:44:23 +0700

    Resolve Conflict (Gatau bener atau engga wkwk)

--- DM.R
+++ DM.R
@@ -68,11 +68,6 @@ names(dataset)
 # Define the categorical columns where "unknown" values may exist
 categorical_columns <- c("job", "housing", "y")
 
-# Check the column that has alot of uknowns
-# for (col in categorical_columns) {
-#   print(paste("Column:", col))
-#   print(table(dataset[[col]]))
-# }
 
 # Loop through each categorical column and remove rows with "unknown" values
 for (col in categorical_columns) {
@@ -141,28 +136,22 @@ dataset_no <- dataset[dataset$y == 0, ]
 
 
 # Removing y as we dont need it anymore
-# dataset <- subset(dataset, select = -y)
 dataset_yes <- subset(dataset_yes, select = -y)
 dataset_no <- subset(dataset_no, select = -y)
 
-# dataset <- dataset[!duplicated(dataset),]
-# dataset <- dataset[!duplicated(dataset),]
 dataset_yes <- dataset_yes[!duplicated(dataset_yes),]
 dataset_no <- dataset_no[!duplicated(dataset_no),]
 
 # Should be 73945
-# length(dataset$age)
 length(dataset_yes$age)
 length(dataset_no$age)
 
-# write.csv(dataset, file = "Combine.csv", row.names = FALSE)
 write.csv(dataset_yes, file = "Combine_yes.csv", row.names = FALSE)
 write.csv(dataset_no, file = "Combine_no.csv", row.names = FALSE)
 
 # -----------------------------Kebawah belum diganti--------------------------------
 
 # Reading the Combine CSV for better consistency
-# dataset = read.csv("Combine.csv")
 dataset_yes = read.csv("Combine_yes.csv")
 dataset_no = read.csv("Combine_no.csv")
 
@@ -189,20 +178,15 @@ dataset_yes$cluster <- kmeans_yes$cluster
 kmeans_no <- kmeans(dataset_no, centers = 4, nstart = 25)
 dataset_no$cluster <- kmeans_no$cluster
 
-# pca_result <- prcomp(dataset, center = TRUE, scale. = TRUE)
 pca_resultyes <- prcomp(dataset_yes, center = TRUE, scale. = TRUE)
 pca_resultno <- prcomp(dataset_no, center = TRUE, scale. = TRUE)
+
 
 
 #PCA
 
 
 # Correlation Test
-# install.packages("corrplot")
-# library(corrplot)
-# correlate <- cbind(dataset, pca_result$x)
-# cor_matrix <- cor(correlate)
-# corrplot(cor_matrix, method = "color", type = "upper", tl.col = "black", tl.srt = 45)
 
 #install.packages("corrplot")
 library(corrplot)
@@ -216,16 +200,7 @@ correlate_no <- cbind(dataset_no, pca_resultno$x)
 cor_matrix_no <- cor(correlate_no)
 corrplot(cor_matrix_no, method = "color", type = "upper", tl.col = "black", tl.srt = 45)
 
-# # Add PCA scores and the cluster result to a new DF
-# dataset_with_pca <- cbind(cluster = kmeans_result$cluster, pca_result$x)
-# dataset_with_pca <- as.data.frame(dataset_with_pca)
-
-# # Remove unused PC
-# dataset_with_pca = subset(dataset_with_pca, select = -PC4)
-# dataset_with_pca = subset(dataset_with_pca, select = -PC5)
-# dataset_with_pca = subset(dataset_with_pca, select = -PC6)
-
-# Combine only PC1, PC2, and PC3 with the cluster results for yes an no data (shortcut)
+# Combine only PC1, PC2, and PC3 with the cluster results for yes an no data 
 datasetyes_with_pca <- cbind(cluster = kmeans_yes$cluster, pca_resultyes$x[, 1:3])
 datasetyes_with_pca <- as.data.frame(datasetyes_with_pca)
 datasetno_with_pca <- cbind(cluster = kmeans_no$cluster, pca_resultno$x[, 1:3])
@@ -235,16 +210,6 @@ datasetno_with_pca <- as.data.frame(datasetno_with_pca)
 #install.packages("plotly")
 # Load the plotly library
 library(plotly)
-
-# 3D Scatter plot
-#  p <- plot_ly(dataset_with_pca, x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(cluster), colors = "Set1") %>%
-#   add_markers() %>%
-#   layout(scene = list(
-#     xaxis = list(title = 'PC1'),
-#     yaxis = list(title = 'PC2'),
-#     zaxis = list(title = 'PC3')),
-#     title = "3D PCA Plot by Cluster"
-#   )
 
 # 3D plot for "yes" data using PCA components
 p_yes <- plot_ly(datasetyes_with_pca , x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(cluster), colors = "Set1") %>%
@@ -276,74 +241,165 @@ p_no
 htmlwidgets::saveWidget(p_yes, "3d_pca_plot_yes.html")
 htmlwidgets::saveWidget(p_no, "3d_pca_plot_no.html")
 
-# # DBSCAN
-# install.packages("dbscan")
+# Install the 'dbscan' package if you haven't already
+install.packages("dbscan")
+
+# Load the DBSCAN library
 library(dbscan)
 dataset_yes = read.csv("Combine_yes.csv")
 dataset_no = read.csv("Combine_no.csv")
-
+# DBSCAN Clustering for "Yes" Data
 dbscan_yes <- dbscan(dataset_yes, eps = 0.5, minPts = 5)
-dataset_yes$cluster <- dbscan_yes$cluster
+datasetyes_with_pca$dbscan_cluster <- dbscan_yes$cluster
+
+# DBSCAN Clustering for "No" Data
 dbscan_no <- dbscan(dataset_no, eps = 0.5, minPts = 5)
-dataset_no$cluster <- dbscan_no$cluster
+datasetno_with_pca$dbscan_cluster <- dbscan_no$cluster
 
-pca_resultyes <- prcomp(dataset_yes, center = TRUE, scale. = TRUE)
-pca_resultno <- prcomp(dataset_no, center = TRUE, scale. = TRUE)
+# Install the 'mclust' package if you haven't already
+#install.packages("mclust")
 
-library(corrplot)
-#corr for yes data
-correlate_yes <- cbind(dataset_yes, pca_resultyes$x)
-cor_matrix_yes <- cor(correlate_yes)
-corrplot(cor_matrix_yes, method = "color", type = "upper", tl.col = "black", tl.srt = 45)
+# Load the Mclust library
+library(mclust)
+dataset_yes = read.csv("Combine_yes.csv")
+dataset_no = read.csv("Combine_no.csv")
+# GMM Clustering for "Yes" Data
+gmm_yes <- Mclust(dataset_yes)
+datasetyes_with_pca$gmm_cluster <- gmm_yes$classification
 
-#corr for no data
-correlate_no <- cbind(dataset_no, pca_resultno$x)
-cor_matrix_no <- cor(correlate_no)
-corrplot(cor_matrix_no, method = "color", type = "upper", tl.col = "black", tl.srt = 45)
+# GMM Clustering for "No" Data
+gmm_no <- Mclust(dataset_no)
+datasetno_with_pca$gmm_cluster <- gmm_no$classification
 
-# Combine only PC1, PC2, and PC3 with the cluster results for yes an no data (shortcut)
-datasetyes_with_pca <- cbind(cluster = dbscan_yes$cluster, pca_resultyes$x[, 1:3])
-datasetyes_with_pca <- as.data.frame(datasetyes_with_pca)
-datasetno_with_pca <- cbind(cluster = dbscan_no$cluster, pca_resultno$x[, 1:3])
-datasetno_with_pca <- as.data.frame(datasetno_with_pca)
-
-plot_ly(datasetyes_with_pca , x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(cluster), colors = "Set1") %>%
+# 3D plot for DBSCAN Clustering ("Yes" data)
+p_yes_dbscan <- plot_ly(datasetyes_with_pca, x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(dbscan_cluster), colors = "Set1") %>%
   add_markers() %>%
   layout(scene = list(
     xaxis = list(title = 'PC1'),
     yaxis = list(title = 'PC2'),
     zaxis = list(title = 'PC3')),
-    title = "3D PCA Plot for 'Yes' Data"
+    title = "3D DBSCAN Plot for 'Yes' Data"
   )
 
-plot_ly(datasetno_with_pca, x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(cluster), colors = "Set1") %>%
+# Show the plot for DBSCAN 'Yes' data
+p_yes_dbscan
+
+# 3D plot for DBSCAN Clustering ("No" data)
+p_no_dbscan <- plot_ly(datasetno_with_pca, x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(dbscan_cluster), colors = "Set1") %>%
   add_markers() %>%
   layout(scene = list(
     xaxis = list(title = 'PC1'),
     yaxis = list(title = 'PC2'),
     zaxis = list(title = 'PC3')),
-    title = "3D PCA Plot for 'No' Data"
+    title = "3D DBSCAN Plot for 'No' Data"
   )
-# set.seed(4920)
-# dbscan_result <- dbscan(dataset, eps = 0.5, minPts = 5)
-# dataset <- cbind(dataset, DBSCAN_Cluster = dbscan_result$cluster)
-# unique(dataset$DBSCAN_Cluster)
-# dataset_with_dbscanpca <- cbind(cluster = dbscan_result$cluster, pca_result$x)
-# dataset_with_dbscanpca <- as.data.frame(dataset_with_dbscanpca)
 
-# # Remove unused PC
-# dataset_with_dbscanpca = subset(dataset_with_dbscanpca, select = -PC4)
-# dataset_with_dbscanpca = subset(dataset_with_dbscanpca, select = -PC5)
-# dataset_with_dbscanpca = subset(dataset_with_dbscanpca, select = -PC6)
+# Show the plot for DBSCAN 'No' data
+p_no_dbscan
 
-# plot_ly(dataset_with_dbscanpca, 
-#         x = ~PC1, y = ~PC2, z = ~PC3, 
-#         color = ~as.factor(cluster), 
-#         colors = "Set1") %>%
-#   add_markers() %>%
-#   layout(scene = list(
-#     xaxis = list(title = 'PC1'),
-#     yaxis = list(title = 'PC2'),
-#     zaxis = list(title = 'PC3')),
-#     title = "3D PCA Plot by DBSCAN Cluster"
-#   )
+# 3D plot for GMM Clustering ("Yes" data)
+p_yes_gmm <- plot_ly(datasetyes_with_pca, x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(gmm_cluster), colors = "Set1") %>%
+  add_markers() %>%
+  layout(scene = list(
+    xaxis = list(title = 'PC1'),
+    yaxis = list(title = 'PC2'),
+    zaxis = list(title = 'PC3')),
+    title = "3D GMM Plot for 'Yes' Data"
+  )
+
+# Show the plot for GMM 'Yes' data
+p_yes_gmm
+
+# 3D plot for GMM Clustering ("No" data)
+p_no_gmm <- plot_ly(datasetno_with_pca, x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(gmm_cluster), colors = "Set1") %>%
+  add_markers() %>%
+  layout(scene = list(
+    xaxis = list(title = 'PC1'),
+    yaxis = list(title = 'PC2'),
+    zaxis = list(title = 'PC3')),
+    title = "3D GMM Plot for 'No' Data"
+  )
+
+# Show the plot for GMM 'No' data
+p_no_gmm
+
+# Save the results for DBSCAN clustering
+write.csv(dataset_yes, file = "Dataset_yes_with_DBSCAN.csv", row.names = FALSE)
+write.csv(dataset_no, file = "Dataset_no_with_DBSCAN.csv", row.names = FALSE)
+
+# Save the results for GMM clustering
+write.csv(dataset_yes, file = "Dataset_yes_with_GMM.csv", row.names = FALSE)
+write.csv(dataset_no, file = "Dataset_no_with_GMM.csv", row.names = FALSE)
+
+#coba
+# Install necessary packages if you don't have them
+install.packages("kohonen")  # For Self-Organizing Maps (SOM)
+install.packages("cluster")  # For Hierarchical Clustering
+install.packages("factoextra")  # For better visualization of Hierarchical Clustering
+
+library(kohonen)  # For SOM
+library(cluster)   # For Hierarchical Clustering
+library(factoextra)  # For visualizing Hierarchical Clustering
+
+
+# ---------------------- HIERARCHICAL CLUSTERING ----------------------
+
+# Hierarchical clustering for "Yes" data
+dist_yes <- dist(dataset_yes)  # Compute the distance matrix
+hc_yes <- hclust(dist_yes, method = "ward.D2")  # Perform hierarchical clustering
+dataset_yes$hclust_cluster <- cutree(hc_yes, k = 4)  # Cut the dendrogram to form 4 clusters
+
+# Hierarchical clustering for "No" data
+dist_no <- dist(dataset_no)  # Compute the distance matrix
+hc_no <- hclust(dist_no, method = "ward.D2")  # Perform hierarchical clustering
+dataset_no$hclust_cluster <- cutree(hc_no, k = 4)  # Cut the dendrogram to form 4 clusters
+
+# Visualizing Hierarchical Clustering for "Yes" data
+fviz_dend(hc_yes, rect = TRUE, k = 4, main = "Hierarchical Clustering Dendrogram (Yes Data)")
+
+# Visualizing Hierarchical Clustering for "No" data
+fviz_dend(hc_no, rect = TRUE, k = 4, main = "Hierarchical Clustering Dendrogram (No Data)")
+
+
+# Adding Hierarchical Clustering and SOM results to the dataset
+
+# For "Yes" data
+datasetyes_with_pca$hclust_cluster <- as.factor(dataset_yes$hclust_cluster)
+datasetyes_with_pca$som_cluster <- as.factor(dataset_yes$som_cluster)
+
+# For "No" data
+datasetno_with_pca$hclust_cluster <- as.factor(dataset_no$hclust_cluster)
+datasetno_with_pca$som_cluster <- as.factor(dataset_no$som_cluster)
+
+
+# ---------------------- HIERARCHICAL CLUSTERING 3D PLOT ----------------------
+
+# 3D Plot for Hierarchical Clustering (Yes data)
+# Create a 3D plot using plotly for "Yes" data
+p_hc_yes <- plot_ly(datasetyes_with_pca, x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(hclust_cluster), colors = "Set1") %>%
+  add_markers() %>%
+  layout(
+    title = "3D Hierarchical Clustering (Yes Data)",  # Set title here
+    scene = list(
+      xaxis = list(title = 'PC1'),
+      yaxis = list(title = 'PC2'),
+      zaxis = list(title = 'PC3')
+    )
+  )
+
+# Show the plot
+p_hc_yes
+
+
+# 3D Plot for Hierarchical Clustering (No data)
+# Create a 3D plot using plotly for "No" data
+p_hc_no <- plot_ly(datasetno_with_pca, x = ~PC1, y = ~PC2, z = ~PC3, color = ~as.factor(hclust_cluster), colors = "Set1") %>%
+  add_markers() %>%
+  layout(scene = list(
+    xaxis = list(title = 'PC1'),
+    yaxis = list(title = 'PC2'),
+    zaxis = list(title = 'PC3')
+  ), title = "3D Hierarchical Clustering (No Data)")
+
+# Show the plot
+p_hc_no
